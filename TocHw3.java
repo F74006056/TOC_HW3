6f3446b7e45523a1fcbf5fe615e5eb357aefb3b1
F74006056 <thyouopop@tp.edu.tw>
2014-06-12 00:20:05 +0800

    TOC

--- TocHw3.java
+++ TocHw3.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
-public class TocHW3 {
+public class TocHw3 {
 
 	public static void main(String[] args) 
 	{
@@ -240,7 +240,7 @@ public class TocHW3 {
 			}
 			for(int i=0;i<store.size();++i)
 				average+=store.elementAt(i).price;
-			average=(int)((float)average/store.size());
+			average=(int)(average/store.size());
 			System.out.println(average);
 			br.close();
 		}
